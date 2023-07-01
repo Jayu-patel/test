e38a66b9eb6973f2e43361d0e98c8a2327f725dc
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-07-01 12:04:32 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const data = require('./data')
+const cors = require('cors')
 
+app.use(cors())
 app.get('/data',(req,res)=>{
     res.send(data)
 })
+
 app.listen(5000)
