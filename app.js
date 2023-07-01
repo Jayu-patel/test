b8479cfb8847ac528d001ee833ede5d358bd3efb
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2023-07-01 12:59:53 +0530

    Update port

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const data = require('./data')
 const cors = require('cors')
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.get('/data',(req,res)=>{
     res.send(data)
 })
 
-app.listen(5000)
+app.listen(port)
